e064491a164c2e44025de605cb971afdac851a4b
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-01-18 11:42:45 +0400

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
@@ -2,31 +2,27 @@
 
 class Solution {
     public int longestSubarray(int[] arr, int k) {
-        // code herefor
+        // code here
         int n=arr.length;
-        int pp[]=new int[n];
-        int maxl=0;
-        pp[0]=arr[0];
-        for(int i=1;i<arr.length;i++){
-            pp[i]=pp[i-1]+arr[i];
+        int p[]=new int [n];
+        int max=0;
+        p[0]=arr[0];
+        for(int i=1;i<n;i++){
+            p[i]=p[i-1]+arr[i];
             
         }
-        HashMap<Integer,Integer>mm=new HashMap<>();
-        for(int j=0;j<arr.length;j++){
-            if(pp[j]==k)maxl=Math.max(maxl,j+1);
-            int val=pp[j]-k;
-            if(mm.containsKey(val)){
-                maxl=Math.max(maxl,j-mm.get(val));
+        HashMap<Integer,Integer>ss=new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(p[i]==k)max=Math.max(max,i+1);
+            int val=p[i]-k;
+            if(ss.containsKey(val)){
+                max=Math.max(max,i-ss.get(val));
                 
             }
-        
-        
-        if(!mm.containsKey(pp[j])){
-            mm.put(pp[j],j);
+            if(!ss.containsKey(p[i])){
+                ss.put(p[i],i);
+            }
         }
-        }
-        return maxl;
-        
-        
+        return max;
     }
 }
