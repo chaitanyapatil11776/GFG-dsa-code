e370d7c30ce493b75387813e7d32e468c17e4ec5
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-01-19 15:14:42 +0400

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Largest subarray of 0's and 1's/largest-subarray-of-0s-and-1s.java
+++ Difficulty: Easy/Largest subarray of 0's and 1's/largest-subarray-of-0s-and-1s.java
@@ -1,26 +1,24 @@
 class Solution {
     public int maxLen(int[] arr) {
-        // Your code her
+        // Your code here
         int n=arr.length;
-        int maxl=0;
-        
         int p[]=new int[n];
-        p[0]=(arr[0]==0)?-1:1;
+        int max=0;
+        p[0]=((arr[0]==0)?-1:1);
+        HashMap<Integer,Integer>ss=new HashMap<>();
+        ss.put(0,1);
         for(int i=1;i<n;i++){
             p[i]=p[i-1]+((arr[i]==0)?-1:1);
         }
-        HashMap<Integer,Integer>mm=new HashMap<>();
-        for(int j=0;j<n;j++){
-            if(p[j]==0)maxl=j+1;
-            
-            if(mm.containsKey(p[j])){
-                maxl=Math.max(maxl,j-mm.get(p[j]));
+        for(int i=0;i<n;i++){
+            if(p[i]==0)max=Math.max(max,i+1);
+            if(ss.containsKey(p[i])){
+                max=Math.max(max,i-ss.get(p[i]));
             }
-            else
-                mm.put(p[j],j);
-            
+            if(!ss.containsKey(p[i])){
+                ss.put(p[i],i);            }
         }
-        return maxl;
         
+        return max;
     }
 }
