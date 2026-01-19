19222b899865a44082a6fcb80266961d1d2caef2
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-01-19 20:08:00 +0400

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check if two strings are same or not/check-if-two-strings-are-same-or-not.java
+++ Difficulty: Easy/Check if two strings are same or not/check-if-two-strings-are-same-or-not.java
@@ -3,11 +3,12 @@ class StringUtils {
     // Function to check if two strings are the same
     public static boolean areStringsSame(String s1, String s2) {
         // code here
-        if(s1.length()!=s2.length())return false;
-    for(int i=0;i<s1.length();i++){
-        if(s1.charAt(i)!=s2.charAt(i))return false;
-    }
-        return true;
         
+        if(s1.length()!=s2.length())return false;
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i)!=s2.charAt(i))return false;
+            
+        }
+        return true;
     }
 }
