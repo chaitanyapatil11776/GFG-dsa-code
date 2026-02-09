afbcec2f37f8733e77ca85c59ee17f54ba280a12
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-02-09 09:36:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Segregate Even and Odd numbers/segregate-even-and-odd-numbers.java
+++ Difficulty: Basic/Segregate Even and Odd numbers/segregate-even-and-odd-numbers.java
@@ -1,25 +1,23 @@
 class Solution {
     void segregateEvenOdd(int arr[]) {
         // code here
-        int temp[]=new int[arr.length];
-   Arrays.sort(arr);
-   int index=0;
-   
-   for(int i=0;i<arr.length;i++){
-       if(arr[i]%2==0){
-           temp[index]=arr[i];
-           index++;
-       }
-   }
-   for(int i=0;i<arr.length;i++){
-       if(arr[i]%2!=0){
-           temp[index]=arr[i];
-           index++;
-       }
-   }
+        int []temp=new int[arr.length];
+        Arrays.sort(arr);
+        int index=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                temp[index++]=arr[i];
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2!=0){
+                temp[index++]=arr[i];
+                
+            }
+            
+        }
         for(int i=0;i<arr.length;i++){
             arr[i]=temp[i];
         }
-    
     }
 }
