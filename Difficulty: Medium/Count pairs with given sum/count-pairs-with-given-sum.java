cf1f6d5aa1f4020b71259178793eea793ea8a302
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-02-14 13:53:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count pairs with given sum/count-pairs-with-given-sum.java
+++ Difficulty: Medium/Count pairs with given sum/count-pairs-with-given-sum.java
@@ -1,17 +1,16 @@
 class Solution {
     int countPairs(int arr[], int target) {
         // code here
-        int count=0;
         HashMap<Integer,Integer>ss=new HashMap<>();
+        int count=0;
         for(int i=0;i<arr.length;i++){
+            // if()
             int diff=target-arr[i];
             if(ss.containsKey(diff)){
                 count+=ss.get(diff);
             }
-            
             ss.put(arr[i],ss.getOrDefault(arr[i],0)+1);
         }
         return count;
-        
     }
 }
