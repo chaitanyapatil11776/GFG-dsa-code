0ed47c11b10b2fc9c2380a2d69f5b7f8d4c4af85
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-03-23 09:26:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
@@ -1,23 +1,52 @@
-// User function Template for Java
-
-class GFG {
+class Solution {
     ArrayList<Integer> find(int arr[], int x) {
         // code here
-        
-        ArrayList<Integer>re=new ArrayList<>();
-        int f=-1;
-        int l=-1;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==x){
-                if(f==-1){
-                    f=i;
-                }l=i;
-            }
-            
-          
-        }
-        re.add(f);
-        re.add(l);
-        return re;
+        int a=ff(arr,x);
+        int b=fl(arr,x);
+        ArrayList<Integer>rr=new ArrayList<>();
+        rr.add(a);
+        rr.add(b);
+        return rr;
     }
+     int ff(int arr[],int x){
+         int low=0;
+         int ans=-1;
+         int hight=arr.length-1;
+         while(low<=hight){
+             int mid=low+(hight-low)/2;
+             if(arr[mid]==x){
+                //  return mid;
+                ans=mid;
+                hight=mid-1;
+                 
+             }else if(arr[mid]<x){
+                 low=mid+1;
+             }else{
+                 hight=mid-1;
+             }
+         }
+         return ans;
+     }
+     
+     
+     int fl(int arr[],int x){
+         int low=0;
+         int ans=-1;
+         int hight=arr.length-1;
+         while(low<=hight){
+             int mid=low+(hight-low)/2;
+             if(arr[mid]==x){
+                //  return mid;
+                ans=mid;
+                // hight=mid-1;
+            low=mid+1;
+                 
+             }else if(arr[mid]<x){
+                 low=mid+1;
+             }else{
+                 hight=mid-1;
+             }
+         }
+         return ans;
+     }
 }
