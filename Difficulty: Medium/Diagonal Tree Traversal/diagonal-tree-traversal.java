fe525307b8101763e9b73a5507477cded15c8723
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-03-26 10:03:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Diagonal Tree Traversal/diagonal-tree-traversal.java
+++ Difficulty: Medium/Diagonal Tree Traversal/diagonal-tree-traversal.java
@@ -11,19 +11,23 @@ class Node{
 }*/
 class Tree {
     public ArrayList<Integer> diagonal(Node root) {
-        // add your code here.
+        // add your code here.ge
+        
         ArrayList<Integer>ss=new ArrayList<>();
-         if (root == null) return ss;
+        if(root==null)return ss;
         Queue<Node>que=new LinkedList<>();
         que.add(root);
         while(!que.isEmpty()){
-            Node curr=que.poll();
-            while(curr!=null){
-                ss.add(curr.data);
-                if(curr.left!=null){
-                    que.add(curr.left);
+            int size=que.size();
+            for(int i=0;i<size;i++){
+                Node curr=que.poll();
+                while(curr!=null){
+                    ss.add(curr.data);
+                    if(curr.left!=null){
+                        que.add(curr.left);
+                    }
+                    curr=curr.right;
                 }
-                curr=curr.right;
             }
         }
         return ss;
