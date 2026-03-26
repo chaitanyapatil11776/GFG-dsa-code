daee83ac1e2a62070903bb5eb57e07eb5c9266d2
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-03-26 14:20:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/k-th Smallest in BST/kth-smallest-in-bst.java
+++ Difficulty: Medium/k-th Smallest in BST/kth-smallest-in-bst.java
@@ -1,38 +1,23 @@
-/*
-class Node {
-    int data;
-    Node left, right;
-
-    public Node(int d)
-    {
-        data = d;
-        left = right = null;
-    }
-}
-*/
-
 class Solution {
-    
-    int count=0;
-    int asn=-1;
-    void ss(Node root,int k){
-        if(root==null)return ;
-        ss(root.left,k);
+    int count = 0;
+    int res = -1;
+
+    void ss(Node root, int k) {
+        if (root == null) return;
+
+        ss(root.left, k);
+
         count++;
-        if(count==k){
-            asn=root.data;
-            return ;
+        if (count == k) {
+            res = root.data;
+            return;
         }
-        ss(root.right,k);
-        
+
+        ss(root.right, k);
     }
-    
+
     public int kthSmallest(Node root, int k) {
-        // code here
-        count=0;
-        asn=-1;
-        ss(root,k);
-        return asn;
-        
+        ss(root, k);
+        return res;
     }
 }
