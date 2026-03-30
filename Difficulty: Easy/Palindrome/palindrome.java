2f88d2892d595dbfce1c2fd2dffb6541cd02740a
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-03-30 12:16:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome/palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int n) {
+        // code here
+        
+        if(n<0 || (n%10==0 && n!=0))return false;
+        int rev=0;
+        while(n>rev){
+            rev=rev*10+(n%10);
+            n/=10;
+        }
+        return n==rev || n==rev/10;
+    }
+}
