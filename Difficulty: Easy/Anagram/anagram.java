531583b676493ac148630abfcaa50c53cf2d4ed4
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-03-30 20:50:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -3,15 +3,14 @@ class Solution {
         // code here
         if(s1.length()!=s2.length())return false;
         HashMap<Character,Integer>ss=new HashMap<>();
-        for(char c:s1.toCharArray
-        ()){
+        for(char c:s1.toCharArray()){
             ss.put(c,ss.getOrDefault(c,0)+1);
         }
-        for(char c:s2.toCharArray()){
+          for(char c:s2.toCharArray()){
             ss.put(c,ss.getOrDefault(c,0)-1);
         }
-        for(int val:ss.values()){
-            if(val!=0)return false;
+        for(int cc:ss.values()){
+            if(cc!=0)return false;
         }
         return true;
     }
