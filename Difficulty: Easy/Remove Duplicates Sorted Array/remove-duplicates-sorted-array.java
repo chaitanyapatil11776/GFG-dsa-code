3f50487b7916e96632644d35a62e68a5cfa3fe8d
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-06-11 22:58:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.java
+++ Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.java
@@ -2,10 +2,10 @@ class Solution {
     ArrayList<Integer> removeDuplicates(int[] arr) {
         // code here
         ArrayList<Integer>ss=new ArrayList<>();
-        ss.add(arr[0]);
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]!=arr[i-1]){
-                ss.add(arr[i]);
+        HashSet<Integer>aa=new HashSet<>();
+        for(int i:arr){
+            if(aa.add(i)){
+                ss.add(i);
             }
         }
         return ss;
