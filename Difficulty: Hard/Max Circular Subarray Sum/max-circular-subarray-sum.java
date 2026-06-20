35a30aeb41d0f3e896e7e7b062dafe268ad5c35b
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-06-20 17:37:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.java
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.java
@@ -1,28 +1,25 @@
 class Solution {
     public int maxCircularSum(int arr[]) {
-
-        int total = arr[0];
-
-        int maxcurr = arr[0];
-        int maxsum = arr[0];
-
-        int mincurr = arr[0];
-        int minsum = arr[0];
-
-        for (int i = 1; i < arr.length; i++) {
-            total += arr[i];
-
-            maxcurr = Math.max(arr[i], maxcurr + arr[i]);
-            maxsum = Math.max(maxsum, maxcurr);
-
-            mincurr = Math.min(arr[i], mincurr + arr[i]);
-            minsum = Math.min(minsum, mincurr);
+        // code here
+        int max=arr[0];
+        int min=arr[0];
+        int total=0;
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if(sum>max)max=sum;
+            if(sum<0)sum=0;
+            total+=arr[i];
         }
-
-        // All elements are negative
-        if (maxsum < 0)
-            return maxsum;
-
-        return Math.max(maxsum, total - minsum);
+        
+        sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if(sum<min)min=sum;
+            if(sum>0)sum=0;
+            
+        }
+        if(max<0)return max;
+        return Math.max(max,(total-min));
     }
 }
