d61f63e77576be7615a7e9d69b412fecb7684ead
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-06-23 16:01:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Reverse each word in a given string/reverse-each-word-in-a-given-string.java
+++ Difficulty: Medium/Reverse each word in a given string/reverse-each-word-in-a-given-string.java
@@ -1,13 +1,14 @@
 class Solution {
     public String reverseWords(String s) {
         // Code here
-     String arr[]=s.trim().split("\\s+");
-     ArrayList<String>ss=new ArrayList<>();
-     for(String p:arr){
-         StringBuilder sb=new StringBuilder(p);
-         ss.add(sb.reverse().toString());
-     }
-     return String.join(" ",ss);
+        String a[]=s.trim().split("\\s+");
+        ArrayList<String>ss=new ArrayList<>();
+        for(String p:a)
+{
+    StringBuilder l=new StringBuilder(p);
+    ss.add(l.reverse().toString());
+}
+return String.join(" ",ss);
+
     }
-    
 }
