494bb5b542b301e69a61894217281d8542415020
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-06-24 23:09:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Frequency of Elements/frequency-of-elements.java
+++ Difficulty: Easy/Frequency of Elements/frequency-of-elements.java
@@ -1,20 +1,19 @@
 class Solution {
     public ArrayList<ArrayList<Integer>> countFreq(int[] arr) {
         // code here
-        
         HashMap<Integer,Integer>ss=new HashMap<>();
-        ArrayList<ArrayList<Integer>>ans=new ArrayList<>();
-        for(int i:arr){
-            ss.put(i,ss.getOrDefault(i,0)+1);
+        ArrayList<ArrayList<Integer>>pp=new ArrayList<>();
+        for(int a:arr){
+            ss.put(a,ss.getOrDefault(a,0)+1);
         }
-        
         for(int a:ss.keySet()){
             ArrayList<Integer>temp=new ArrayList<>();
             temp.add(a);
             temp.add(ss.get(a));
-            ans.add(temp);
+            pp.add(temp);
         }
+        return pp;
         
-        return ans;
+        
     }
 }
