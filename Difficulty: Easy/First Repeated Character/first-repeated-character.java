2e9835f908ce812854bf59946a7530b196f3b7df
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-06-24 13:45:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/First Repeated Character/first-repeated-character.java
+++ Difficulty: Easy/First Repeated Character/first-repeated-character.java
@@ -1,15 +1,15 @@
-// User function Template for Java
 class Solution {
     String firstRepChar(String s) {
         // code here
-        HashSet<Character >ss=new HashSet<>();
-        for(char c:s.toCharArray() ){
+        HashSet<Character>ss=new HashSet<>();
+        for(char c:s.toCharArray()){
             if(ss.contains(c)){
                 return String.valueOf(c);
             }
             ss.add(c);
         }
-        return "-1";
+            return "-1";
+        
         
     }
 }
