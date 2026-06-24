308fcf532f859dc83d7d02a2fdb5ee57110751c6
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-06-24 13:04:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Remove Spaces/remove-spaces.java
+++ Difficulty: Basic/Remove Spaces/remove-spaces.java
@@ -1,14 +1,11 @@
-// User function Template for Java
 class Solution {
-
-    String modify(String s) {
-        // your code here
-        StringBuilder sb=new StringBuilder ();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!= ' '){
-                sb.append(s.charAt(i));
+    String removeSpaces(String s) {
+        // code here
+        StringBuilder sb=new StringBuilder();
+        for(char a:s.toCharArray()){
+            if(a!=' '){
+            sb.append(a);
             }
-            
         }
         return sb.toString();
     }
