3cba91e1356716e300985e0d893e049b4ffe2894
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-08-10 12:41:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.java
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.java
@@ -1,29 +1,29 @@
-/*
+/* Structure of Binary Tree Node
 class Node {
     int data;
     Node left, right;
 
     Node(int val) {
         this.data = val;
-        this.left = null;
-        this.right = null;
+        this.left = this.right = null;
     }
-}
-*/
+}*/
 
 class Solution {
     public ArrayList<Integer> leftView(Node root) {
         // code here
         ArrayList<Integer>ss=new ArrayList<>();
-        ss(root,0,ss);
+        sss(root,0,ss);
         return ss;
+        
     }
-    void ss(Node root,int level,ArrayList<Integer>ans){
+    void sss(Node root,int level,ArrayList<Integer>ss){
         if(root==null)return ;
-        if(level==ans.size()){
-            ans.add(root.data);
-        }
-        ss(root.left,level+1,ans);
-        ss(root.right,level+1,ans);
+        if(level==ss.size())ss.add(root.data);
+        sss(root.left,level+1,ss);
+        sss(root.right,level+1,ss);
+      
+        
+        
     }
 }
