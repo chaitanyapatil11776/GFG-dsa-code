054a36bf8978d23d0da7395e5d6107087ce7e3a7
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-08-12 18:31:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.java
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -1,25 +1,18 @@
 class Solution {
-	public ArrayList<Integer> nextLargerElement(int[] arr) {
-		// code here
-		
-		Stack<Integer>ss = new Stack<>();
-		ArrayList<Integer>ans = new ArrayList<>();
-		for (int i = 0; i<arr.length; i++) {
-			ans.add(-1);
-		}
-		
-		for (int i = 0; i<arr.length; i++) {
-			while (!ss.isEmpty () && arr[ss.peek()]<arr[i]) {
-				int index = ss.pop();
-				ans.set(index, arr[i]);
-				
-			}
-			ss.push(i);
-			
-			
-			
-			
-		}
-		return ans;
-	}
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        // code here
+        Stack<Integer>ss=new Stack<>();
+        ArrayList<Integer>ans=new ArrayList<>();
+        for(int a:arr){
+            ans.add(-1);
+        }
+        for(int i=0;i<arr.length;i++){
+            while(!ss.isEmpty()  && arr[ss.peek()]<arr[i]){
+                int index=ss.pop();
+                ans.set(index,arr[i]);
+            }
+            ss.push(i);
+        }
+return ans;
+    }
 }
