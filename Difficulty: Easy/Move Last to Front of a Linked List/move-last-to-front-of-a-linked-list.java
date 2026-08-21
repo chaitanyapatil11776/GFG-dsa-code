e676dbc0b9f87cf5f675f713ddef4369fdf7999b
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-08-21 23:38:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Move Last to Front of a Linked List/move-last-to-front-of-a-linked-list.java
+++ Difficulty: Easy/Move Last to Front of a Linked List/move-last-to-front-of-a-linked-list.java
@@ -13,14 +13,14 @@ class Node
 class Solution {
     public static Node moveToFront(Node head) {
         // code here
-        if(head==null || head.next==null )return head;
-        Node prv=null;
+        if(head==null || head.next==null)return head;
         Node curr=head;
+        Node prv=null;
         while(curr.next!=null){
+           
             prv=curr;
             curr=curr.next;
         }
-        
         prv.next=null;
         curr.next=head;
         return curr;
