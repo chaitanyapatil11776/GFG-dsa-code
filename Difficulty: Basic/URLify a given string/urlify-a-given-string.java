57bf772678bff14c3c73a86d6acfa1296265c703
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-08-22 13:43:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/URLify a given string/urlify-a-given-string.java
+++ Difficulty: Basic/URLify a given string/urlify-a-given-string.java
@@ -3,12 +3,11 @@ class Solution {
         // code here
         StringBuilder sb=new StringBuilder();
         for(char c:s.toCharArray()){
-            if(c==' '){
-                sb.append("%20");
-
-            }else{
+            if(c==' ')sb.append("%20");
+            else{
                 sb.append(c);
             }
         }return sb.toString();
+        
     }
 }
