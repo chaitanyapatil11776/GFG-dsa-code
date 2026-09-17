f5d993fc8d501460503566df7d0e9047c5a90866
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-09-17 13:56:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Distinct in Array/count-distinct-in-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    static int countDistinct(int arr[]) {
+        // code here
+        HashSet<Integer>ss=new HashSet<>();
+        for(int a:arr){
+            ss.add(a);
+        }
+        
+        
+        return ss.size();
+    }
+}
